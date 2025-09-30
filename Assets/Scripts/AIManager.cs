b1ec68c564d083beda15414da6cf3cc2920a3533
Arthur Spiller <arthur.spiller@epitech.eu>
2025-09-30 19:46:53 +0200

    [+] Added AIManager and AIController to generate and control AI opponents.

---
+++ Assets/Scripts/AIManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class AIManager : MonoBehaviour
+{
+    public Transform[] Waypoints;
+    public Transform[] AISpawnPoints;
+
+    private GameObject[] AIs;
+
+    public GameObject AIPrefab;
+    public GameObject PlayerPrefab;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SpawnAI();
+    }
+
+    private void SpawnAI()
+    {
+        AIs = new GameObject[AISpawnPoints.Length];
+
+        for (int i = 0; i < AISpawnPoints.Length; i++)
+        {
+            AIs[i] = Instantiate(AIPrefab, AISpawnPoints[i]);
+            AIs[i].GetComponent<AIOpponentController>().setWaypoints(Waypoints);
+            AIs[i].GetComponent<AIOpponentController>().setPlayerTransform(PlayerPrefab.GetComponent<Transform>());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
